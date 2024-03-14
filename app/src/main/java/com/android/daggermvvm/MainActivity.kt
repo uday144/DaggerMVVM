b488fa3890a54d02ea021bb254cda998331b618d
uday singh <>
2024-03-14 21:40:17 +1100

    Dummy UI

--- app/src/main/java/com/android/daggermvvm/MainActivity.kt
+++ app/src/main/java/com/android/daggermvvm/MainActivity.kt
@@ -2,10 +2,33 @@ package com.android.daggermvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.android.daggermvvm.viewmodels.MainViewModel
+import com.android.daggermvvm.viewmodels.MainViewModelFactory
+import javax.inject.Inject
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var mainViewModelFactory: MainViewModelFactory
+
+    private val products: TextView
+        get() = findViewById(R.id.products)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (application as FakerApplication).applicationComponent.inject(this)
+
+        mainViewModel = ViewModelProvider(this, mainViewModelFactory).get(MainViewModel::class.java)
+
+        mainViewModel.productsLiveData.observe(this, Observer {
+            products.text =  it.joinToString { x -> x.title + "\n\n" }
+        })
     }
 
 }
